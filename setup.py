fe10653ffee6a30331833f5872f16c70c2106aeb
Malleswari <malleswari.gelli@syngenta.com>
2024-10-09 12:58:12 -0500

    code.ipynb added

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "NYC_Trip",
-    version= "0.0.0",
-    author= "Malleswari",
-    author_email= "malleswari.gelli@syngenta.com",
-    packages= find_packages()
-)
